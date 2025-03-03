504389655e9cecff351234b45930051c6b610658
Muneeb1Geek <m.muneebulrahman@gmail.com>
2025-03-03 15:24:24 +0500

    order changed

--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Tag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TagSeeder extends Seeder
 {
@@ -13,10 +14,13 @@ class TagSeeder extends Seeder
      */
     public function run(): void
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        Tag::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         $tags = [
             ['name' => "Popular"],
-            ['name' => "Best Selling"],
             ['name' => "New Arrival"],
+            ['name' => "Best Selling"],
         ];
 
         foreach($tags as $tag){
